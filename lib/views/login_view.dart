e89b661c6bbd01e9cd6f3af244b303df1c55ae27
mohamed majzoub <mohamed13majzoub@gmail.com>
2023-10-07 14:47:44 +0300

    fifth commit

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -2,9 +2,8 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:flutter_application_1/constants/routes.dart';
+import 'package:flutter_application_1/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -68,8 +67,21 @@ class _LoginViewState extends State<LoginView> {
                 );
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
-                  devtools.log('Ivalid Credentials.');
+                  await showErrorDialog(
+                    context,
+                    'Invalid Credentials',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error:${e.code}',
+                  );
                 }
+              } catch (e) {
+                await showErrorDialog(
+                  context,
+                  'Error:${e.toString()}',
+                );
               }
             },
             child: const Text('Login'),
